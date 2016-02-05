cf84f54965af3a2d1776c558303c6c0ee8f887b3
elf Pavlik <perpetual-tripper@wwelves.org>
2016-02-04 20:08:09 -0600

    switching to rdf-ext

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { promises as jsonld } from 'jsonld'
 import _ from 'lodash'
+import rdf from 'rdf-ext'
+import N3Parser from 'rdf-parser-n3'
+import JsonldParser from 'rdf-parser-jsonld'
+import normalize from 'rdf-normalize'
 
 class Dataset {
   /**
@@ -7,6 +10,10 @@ class Dataset {
    */
   constructor (storage) {
     this.storage = storage
+    this.parsers = {
+      n3: new N3Parser(),
+      jsonld: new JsonldParser()
+    }
   }
 
   /**
@@ -33,7 +40,7 @@ class Dataset {
   getResource (resourceUri) {
     return this.storage.get(resourceUri)
       .then((normalized) => {
-        return jsonld.fromRDF(normalized, { format: 'application/nquads' })
+        return this.parsers.n3.parse(normalized)
       })
   }
 
@@ -42,45 +49,41 @@ class Dataset {
    * @param link
    * @returns {Container}
    */
-  getLinkedContainer (resourceUri, link) {
+  getLinkedContainerUri (resourceUri, link) {
     return this.getResource(resourceUri)
-      .then((resource) => {
-        let reference = _.find(resource, (entity) => {
-          if (link[this.expand('rel')] && entity[this.expand('rel')]) {
-            return entity[this.expand('rel')].indexOf(link[this.expand('rel')] >= 0)
-          } else if (link[this.expand('rev')] && entity[this.expand('rev')]) {
-            return entity[this.expand('rev')].indexOf(link[this.expand('rev')] >= 0)
-          }
-        })
-        if (reference) {
-          return this.getResource(reference['@id'])
+      .then((graph) => {
+        let triple
+        if (link[this.expand('rel')]) {
+          triple = graph.match(null, this.expand('rel'), link[this.expand('rel')]).toArray()[0]
+        } else if (link[this.expand('rev')]) {
+          triple = graph.match(null, this.expand('rel'), link[this.expand('rel')]).toArray()[0]
         } else {
           return Promise.reject('needs rel or rev')
         }
+        return triple.subject.nominalValue
       })
   }
 
   /**
    * @param resourceUri
-   * @param data
+   * @param graph
    * @returns {Uri} - uri of the resource
    */
-  createResource (resourceUri, data) {
-    return this.updateResource(resourceUri, data)
+  createResource (resourceUri, graph) {
+    return this.updateResource(resourceUri, graph)
   }
 
   /**
    * @param resourceUri
-   * @param data
+   * @param graph
    * @returns {Uri}
    */
-  updateResource (resourceUri, data) {
-    return jsonld.normalize(data, { algorithm: 'URDNA2015', format: 'application/nquads' })
-    .then((normalized) => {
-      return this.storage.put(resourceUri, normalized)
-    }).then((hash) => {
-      return resourceUri
-    })
+  updateResource (resourceUri, graph) {
+    let normalized = normalize(graph)
+    return this.storage.put(resourceUri, normalized)
+      .then((hash) => {
+        return resourceUri
+      })
   }
 
   /**
@@ -100,25 +103,27 @@ class Dataset {
     } else if (link[this.expand('rev')]) {
       container[this.expand('rev')] = link[this.expand('rev')]
     }
-    return this.createResource(containerUri, container)
+    return this.parsers.jsonld.parse(container)
+      .then((graph) => {
+        return this.createResource(containerUri, graph)
+      })
   }
 
   /**
    * @param containerUri
    * @param memberUri
    * @returns {Uri} - uri of container
+   * TODO: container[this.expand('hydra:totalItems')]++
    */
   addMemberToContainer (containerUri, memberUri) {
     return this.getResource(containerUri)
-      .then((resource) => {
-        let container = resource[0]
-        if (container[this.expand('ldp:member')]) {
-          container[this.expand('ldp:member')].push({ '@id': memberUri })
-        } else {
-          container[this.expand('ldp:member')] = [ { '@id': memberUri } ]
-        }
-        // TODO: container[this.expand('hydra:totalItems')]++
-        return this.updateResource(containerUri, container)
+      .then((graph) => {
+        let subject = new rdf.NamedNode(containerUri)
+        let predicate = new rdf.NamedNode(this.expand('ldp:member'))
+        let object = new rdf.NamedNode(memberUri)
+        let triple = new rdf.Triple(subject, predicate, object)
+        graph.add(triple)
+        return this.updateResource(containerUri, graph)
       }).then((uri) => {
         return uri
       })
@@ -126,14 +131,14 @@ class Dataset {
 
   /**
    * @param resourceUri
-   * @param data
+   * @param graph
    * @returns {Uri} - uri of updated resource
    */
-  appendToResource (resourceUri, data) {
+  appendToResource (resourceUri, graph) {
     return this.getResource(resourceUri)
-      .then((expanded) => {
-        expanded.push(data)
-        return this.updateResource(resourceUri, expanded)
+      .then((original) => {
+        let merged = original.merge(graph)
+        return this.updateResource(resourceUri, merged)
       })
   }
 }
